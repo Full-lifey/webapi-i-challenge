8cdffccb8b91724a95686509db9227a816b12efd
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-08 13:57:11 -0600

    finished findById get endpoint

--- index.js
+++ index.js
@@ -16,8 +16,41 @@ server.get('/users', (req, res) => {
       res.status(200).json(users);
     })
     .catch(err => {
-      res.status(500).json(err);
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' });
     });
+});
+
+server.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  users
+    .findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
+    });
+});
+
+server.post('/users', (req, res) => {
+  const userInfo = req.body;
+
+  users
+    .insert(userInfo)
+    .then(user => {})
+    .catch();
 });
 
 const port = 5000;
