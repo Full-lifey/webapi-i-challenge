3ccb7d4ea4ed0261daccd8150024cdfc82919c6e
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-07-08 13:37:08 -0600

    initial commit, built server and first get for users

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // implement your API here
+const express = require('express');
+
+const users = require('./data/db.js');
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Get is working!!!');
+});
+
+server.get('/users', (req, res) => {
+  users
+    .find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`Running on port ${port}`));
